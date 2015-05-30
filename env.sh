5e6bfd068b9904dfde76fdd273811243afa2d666
Ryan Huang <ryanhuang@cs.ucsd.edu>
2015-05-30 15:49:11 -0700

    add env to source ld library path and classpath

---
+++ env.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home/jre/lib/server
+export CLASSPATH=$CLASSPATH:$HOME/tachyon/lib/tachyon-client-0.6.4-jar-with-dependencies.jar

77fdf5131c0002c763060d8dbd82aeba34de5b29
Ryan Huang <ryanhuang@cs.ucsd.edu>
2016-11-28 21:22:18 -0800

    update env to find jvm path

--- env.sh
+++ env.sh
@@ -1,30 +1,45 @@
-libjvmpaths=(
-  "/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server" # OpenJDK 1.7
-  "/usr/lib/jvm/java-1.7.0-openjdk/jre/lib/amd64/server" # another OpenJDK 1.7
-  "/usr/lib/jvm/jdk1.7.0_65/jre/lib/amd64/server" # Oracle JDK 1.7
-  "/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home/jre/lib/server" # MacOS
-)
-#clientjarpath=$HOME/tachyon/lib/tachyon-client-0.6.4-jar-with-dependencies.jar
-clientjarpath=$HOME/alluxio/core/client/target/alluxio-core-client-1.2.0-jar-with-dependencies.jar
-
-found=0
-for path in "${libjvmpaths[@]}"
-do
-  if [ -d $path ]; then
-    found=1
-    echo "found JVM path at $path, add to LD_LIBRARY_PATH"
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$path
-    break
+find_libjvm()
+{
+  if [[ -z $JAVA_HOME ]]; then
+    echo "JAVA_HOME not set"
+    exit 1
   fi
-done
-if [ $found -eq 0 ]; then
-  echo "cannot find JVM path, LD_LIBRARY_PATH not updated"
-fi
+  platformstr=`uname`
+  jvmpath=""
+  if [[ "$platformstr" == 'Linux' ]]; then
+    for arch in i386 amd64; do
+      jvmpath="$JAVA_HOME/jre/lib/$arch/server"
+      if [[ -e $jvmpath ]]; then
+        break;
+      else
+        jvmpath=""
+      fi
+    done
+  elif [[ "$platformstr" == 'Darwin' ]]; then
+    jvmpath="$JAVA_HOME/jre/lib/server"
+  fi
+  if [[ -e $jvmpath ]]; then
+    echo "found libjvm path at $jvmpath, add to LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$jvmpath
+  else
+    echo "cannot find JVM path, LD_LIBRARY_PATH not updated"
+  fi
+}
 
-if [ -f $clientjarpath ]; then
-  echo "found alluxio client jar, add to CLASSPATH"
-  export CLASSPATH=$CLASSPATH:$clientjarpath
-else
-  echo "cannot find alluxio client jar, CLASSPATH not updated"
-fi
+find_alluxio()
+{
+  base_dir=$ALLUXIO_HOME
+  if [[ -z $base_dir ]]; then
+    base_dir=$HOME/Project/alluxio/1.2.0 # change if necessary
+  fi
+  clientjarpath=$base_dir/core/client/target/alluxio-core-client-1.2.0-jar-with-dependencies.jar
+  if [ -f $clientjarpath ]; then
+    echo "found alluxio client jar at $clientjarpath, add to CLASSPATH"
+    export CLASSPATH=$CLASSPATH:$clientjarpath
+  else
+    echo "cannot find alluxio client jar, CLASSPATH not updated"
+  fi
+}
 
+find_libjvm
+find_alluxio
